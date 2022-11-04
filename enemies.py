d7d56c33059a11f7f4410d790458fe5f75d11957
Ricardo Bizerra <rblf@cin.ufpe.br>
2022-11-04 09:15:59 -0300

    [fix] corrigindo atributos e métodos para movimentação dos inimigos

--- enemies.py
+++ enemies.py
@@ -16,6 +16,7 @@ class Inimigo(pygame.sprite.Sprite):
         self.rect =  self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0,-10)
         self.obstaculo_sprites = obstaculo_sprites
+        self.direction = pygame.math.Vector2()
         
         # Status do inimigo:
         self.tipo_inimigo = nome_inimigo
@@ -29,48 +30,78 @@ class Inimigo(pygame.sprite.Sprite):
         self.notice_radius = info_tipo['notice_radius']
         self.withdraw_radius = info_tipo['withdraw_radius']
 
-        def get_pos_dir(self,player):
-            enemy_vec = pygame.math.Vector2(self.rect.center)
-            player_vec = pygame.math.Vector2(player.rect.center)
-            distance = (player_vec - enemy_vec).magnitude()
-            if distance == 0:
-                direction = (player_vec - enemy_vec).normalize()
-            else:
-                direction = pygame.math.Vector2()
+    def get_pos_dir(self,player):
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.center)
+        distance = (player_vec - enemy_vec).magnitude()
+        if distance > 0:
+            direction = (player_vec - enemy_vec).normalize()
+        else:
+            direction = pygame.math.Vector2()
 
-            return (distance, direction)
+        return (distance, direction)
 
-        def get_status(self, player):
-            distance = self.get_pos_dir(player)[0]
-            print(distance)
+    def get_status(self, player):
+        distance = self.get_pos_dir(player)[0]
+        print(distance)
 
-            if distance <= self.attack_radius:
-                self.status = 'attack'
+        if distance <= self.attack_radius:
+            self.status = 'attack'
 
-            elif distance <= self.notice_radius:
-                self.status = 'move'
+        elif distance <= self.notice_radius:
+            self.status = 'move'
 
-            elif distance <= self.withdraw_radius:
-                self.status = 'withdraw'
+        elif distance <= self.withdraw_radius:
+            self.status = 'withdraw'
 
-            else:
-                self.status = 'idle'
-        
-        def actions(self, player):
-            if self.status == 'attack':
-                print('attack')
-            elif self.status == 'move':
-                self.direction = get_pos_dir(player)[1]
-            elif self.status == 'withdraw':
-                self.direction = -get_pos_dir(player)[1]
-            else:
-                self.direction = pygame.math.Vector2()
+        else:
+            self.status = 'idle'
+    
+    def actions(self, player):
+        if self.status == 'attack':
+            print('attack')
+        elif self.status == 'move':
+            self.direction = self.get_pos_dir(player)[1]
+        elif self.status == 'withdraw':
+            self.direction = -self.get_pos_dir(player)[1]
+        else:
+            self.direction = pygame.math.Vector2()
+    
+    # movimento e colisão que podem sair ao implentar a classe Entity
+    def move(self, speed):
 
-        def update(self):
-            self.move(self.speed)
+        #CORRIGIR A MOVIMENTAÇÃO QUANDO O PERSONAGEM ANDA NA DIAGONAL
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
 
-        def enemy_update(self,player):
-            self.get_status(player)
-            self.actions(player)
+        self.hitbox.x += self.direction.x * speed
+        self.colisao('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.colisao('vertical')
+        self.rect.center = self.hitbox.center
+
+    def colisao(self, direcao):
+        if direcao == 'horizontal':
+            for sprite in self.obstaculo_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0: #INDO PARA A DIREITA
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0: #INDO PARA A ESQUERDA
+                        self.hitbox.left = sprite.hitbox.right
+
+        if direcao == 'vertical':
+            for sprite in self.obstaculo_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0: #INDO PARA BAIXO
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0: #INDO PARA CIMA
+                        self.hitbox.top = sprite.hitbox.bottom    
+    
+    def update(self):
+        self.move(self.speed)
+
+    def enemy_update(self,player):
+        self.get_status(player)
+        self.actions(player)
         
 
